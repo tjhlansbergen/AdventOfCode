0191f6ee5294ff0ea43a79bb2ac591330eb0d506
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-01-02 20:56:46 +0100

    2015 day 1 part two

--- 2015/day1.py
+++ 2015/day1.py
@@ -2,5 +2,19 @@ import helpers
 
 lines = helpers.list_from_file('2015/inputs/day1')
 
+# part one
 for line in lines:
     print(line.count('(') - line.count(')'))
+
+# part two
+for line in lines:
+    floor = 0
+    index = 0
+
+    for i in line:
+        index += 1
+        floor += 1 if i == "(" else -1
+        if floor == -1:
+            print(index)
+            exit()
+    
