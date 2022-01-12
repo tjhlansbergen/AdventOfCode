1b817ac40fd8f234f6ab3b0fa23396ca14dd2c6a
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-01-12 20:56:18 +0100

    2015 day 1 cs

--- 2015/cs/Day1.cs
+++ 2015/cs/Day1.cs
@@ -4,8 +4,21 @@ namespace Runner
     {
         internal static void Run()
         {
-            var lines = File.ReadAllLines("../inputs/day1");
-            Console.WriteLine(lines[0]);
+            // part one
+            var line = File.ReadAllLines("../inputs/day1")[0];
+            Console.WriteLine(line.Count(c => c == '(') - line.Count(c => c == ')'));
+            
+            // part two
+            var floor = 0;
+            var index = 0;
+            foreach (char c in line)
+            {
+                index += 1;
+                floor += c == '(' ? 1 : -1;
+                if (floor == -1) break; 
+            }
+
+            System.Console.WriteLine(index);
         }
     }
 }
