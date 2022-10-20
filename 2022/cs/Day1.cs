7ee29c4556d89c3570652b9bb2a0332f211fe548
Tako Lansbergen <tako@uptrends.com>
2022-10-20 16:24:45 +0200

    Add new runner

---
+++ 2022/cs/Day1.cs
@@ -0,0 +1,14 @@
+namespace AocRunner;
+
+public class Day1
+{
+    public static void Run(string input, string[] lines)
+    {
+        System.Console.WriteLine(input);
+
+        foreach (var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
