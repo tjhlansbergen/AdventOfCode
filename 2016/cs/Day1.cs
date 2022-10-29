7bc21591031b812d7bc3f879a295015f1282901d
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-10-29 20:11:16 +0200

    Refactor

--- 2016/cs/Day1.cs
+++ 2016/cs/Day1.cs
@@ -5,47 +5,37 @@ public class Day1
     public static void Run(string input, string[] lines)
     {
         var moves = input.Split(',').Select(m => m.Trim()).ToArray();
-        var route = new List<Tuple<int, int>>{new Tuple<int, int>(0,0)};
-
+        var route = new List<Tuple<int, int>> { new Tuple<int, int>(0, 0) };
         int direction = 0; // 0, 90, 180, 270       
 
         foreach (var move in moves)
         {
             var turn = move.Substring(0, 1);
             var steps = int.Parse(move.Substring(1, move.Length - 1));
-            
+
             direction += turn == "R" ? 90 : -90;
             if (direction == 360) direction = 0;
             if (direction == -90) direction = 270;
 
-            switch (direction)
+            for (int i = 0; i < steps; i++)
             {
-                case 0:
-                    for (int i = 0; i < steps; i++)
-                    {
+                switch (direction)
+                {
+                    case 0:
                         route.Add(new Tuple<int, int>(route.Last().Item1, route.Last().Item2 + 1));
-                    }
-                    break;
-                case 90:
-                    for (int i = 0; i < steps; i++)
-                    {
+                        break;
+                    case 90:
                         route.Add(new Tuple<int, int>(route.Last().Item1 + 1, route.Last().Item2));
-                    }
-                    break;
-                case 180:
-                    for (int i = 0; i < steps; i++)
-                    {
+                        break;
+                    case 180:
                         route.Add(new Tuple<int, int>(route.Last().Item1, route.Last().Item2 - 1));
-                    }
-                    break;
-                case 270:
-                    for (int i = 0; i < steps; i++)
-                    {
+                        break;
+                    case 270:
                         route.Add(new Tuple<int, int>(route.Last().Item1 - 1, route.Last().Item2));
-                    }
-                    break;
-                default:
-                    throw new InvalidOperationException($"Invalid direction: {direction}");
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Invalid direction: {direction}");
+                }
             }
         }
 
@@ -53,7 +43,7 @@ public class Day1
         System.Console.WriteLine($"part 1: {Math.Abs(route.Last().Item1) + Math.Abs(route.Last().Item2)}");
 
         // part 2
-        var pos = route.Select((r, i) => new {r, i}).First(r => route.GetRange(0, r.i).Contains(r.r)).r;
+        var pos = route.Select((r, i) => new { r, i }).First(r => route.GetRange(0, r.i).Contains(r.r)).r;
         System.Console.WriteLine($"part 2: {Math.Abs(pos.Item1) + Math.Abs(pos.Item2)}");
-   }
+    }
 }
