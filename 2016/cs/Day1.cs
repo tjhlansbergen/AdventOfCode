1a3349c6fc4cfe52daad25ecf302fe2f74726960
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-10-29 19:12:49 +0200

    2016 day 1 pt 2

--- 2016/cs/Day1.cs
+++ 2016/cs/Day1.cs
@@ -5,15 +5,15 @@ public class Day1
     public static void Run(string input, string[] lines)
     {
         var moves = input.Split(',').Select(m => m.Trim()).ToArray();
-        int x = 0, y = 0;
+        var route = new List<Tuple<int, int>>{new Tuple<int, int>(0,0)};
+
         int direction = 0; // 0, 90, 180, 270       
 
         foreach (var move in moves)
         {
             var turn = move.Substring(0, 1);
             var steps = int.Parse(move.Substring(1, move.Length - 1));
-
-
+            
             direction += turn == "R" ? 90 : -90;
             if (direction == 360) direction = 0;
             if (direction == -90) direction = 270;
@@ -21,22 +21,39 @@ public class Day1
             switch (direction)
             {
                 case 0:
-                    y += steps;
+                    for (int i = 0; i < steps; i++)
+                    {
+                        route.Add(new Tuple<int, int>(route.Last().Item1, route.Last().Item2 + 1));
+                    }
                     break;
                 case 90:
-                    x += steps;
+                    for (int i = 0; i < steps; i++)
+                    {
+                        route.Add(new Tuple<int, int>(route.Last().Item1 + 1, route.Last().Item2));
+                    }
                     break;
                 case 180:
-                    y -= steps;
+                    for (int i = 0; i < steps; i++)
+                    {
+                        route.Add(new Tuple<int, int>(route.Last().Item1, route.Last().Item2 - 1));
+                    }
                     break;
                 case 270:
-                    x -= steps;
+                    for (int i = 0; i < steps; i++)
+                    {
+                        route.Add(new Tuple<int, int>(route.Last().Item1 - 1, route.Last().Item2));
+                    }
                     break;
                 default:
                     throw new InvalidOperationException($"Invalid direction: {direction}");
             }
         }
 
-        System.Console.WriteLine($"part 1: {Math.Abs(x) + Math.Abs(y)}");
-    }
+        // part 1
+        System.Console.WriteLine($"part 1: {Math.Abs(route.Last().Item1) + Math.Abs(route.Last().Item2)}");
+
+        // part 2
+        var pos = route.Select((r, i) => new {r, i}).First(r => route.GetRange(0, r.i).Contains(r.r)).r;
+        System.Console.WriteLine($"part 2: {Math.Abs(pos.Item1) + Math.Abs(pos.Item2)}");
+   }
 }
