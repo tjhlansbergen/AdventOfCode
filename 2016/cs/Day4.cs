9158de13a4bd6e01e0ec626bb3f557ebdc05f211
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-10-30 21:44:06 +0100

    2016 day 4 pt 2

--- 2016/cs/Day4.cs
+++ 2016/cs/Day4.cs
@@ -8,12 +8,41 @@ public class Day4
 
         foreach (var line in lines)
         {
-            var parts = line.Replace("-", "").Split(']')[0].Split('[');
+            var parts = line.Split(']')[0].Split('[');
             var name = parts[0].Substring(0, parts[0].Length - 3);
             var sector = int.Parse(parts[0].Substring(parts[0].Length - 3, 3));
             var checksum = parts[1];
 
-            if (IsValid(name, checksum)) sum += sector;
+            if (IsValid(name, checksum)) 
+            {
+                // pt 1
+                sum += sector;  
+
+                // pt 2
+                var decodedChars = new List<char>();
+                int move = sector % 26;
+
+                foreach (var c in name)
+                {
+                    if (c == '-') 
+                    {
+                        decodedChars.Add(' ');
+                        continue;
+                    }
+
+                    int ascii = (byte)c;
+                    int result = ascii + move;
+                    if (result > 122) result -= 26;
+
+                    decodedChars.Add((char)result);
+                }
+
+                if (string.Concat(decodedChars).Contains("northpole object"))
+                {
+                    System.Console.WriteLine($"Part 2: {sector}");
+                    break;
+                }
+            }
         }
 
         System.Console.WriteLine($"Part 1: {sum}");
@@ -21,7 +50,7 @@ public class Day4
         bool IsValid(string name, string checksum)
         {
             // calculate the full checksum
-            var orderedName = string.Concat(name.GroupBy(c => c).OrderByDescending(g => g.Count()).ThenBy(g => g.Key).Select(g => g.Key));
+            var orderedName = string.Concat(name.Replace("-", "").GroupBy(c => c).OrderByDescending(g => g.Count()).ThenBy(g => g.Key).Select(g => g.Key));
 
             // compare
             return orderedName.Substring(0, 5) == checksum;
