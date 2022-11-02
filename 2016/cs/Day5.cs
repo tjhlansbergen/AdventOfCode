4b4ecdaa03cf7b3abfa12f6135171bc063341ceb
Tako Lansbergen <tako@uptrends.com>
2022-11-02 16:40:45 +0100

    Linkify day 5

--- 2016/cs/Day5.cs
+++ 2016/cs/Day5.cs
@@ -4,39 +4,27 @@ public class Day5
 {
     public static void Run(string input, string[] lines)
     {
-        var index = 0;
-        string hash = string.Empty, passcode = string.Empty;
-
-        var result = new List<char>();
 
         // part 1
         System.Console.WriteLine("\nPart 1: ");
-        do
-        {
-            passcode = input + index.ToString();
-            hash = GenerateMD5(passcode);
+        var sixthChars = Enumerable.Range(0, int.MaxValue)
+            .Select(i => NextHash(input, i))
+            .Where(h => h.StartsWith("00000"))
+            .Select(h => h[5]).Take(8);
+        System.Console.WriteLine(string.Concat(sixthChars));
 
-            if (hash.StartsWith("00000"))
-            {
-                result.Add(hash[5]);
-                System.Console.WriteLine(string.Concat(result));
-            }
-
-            index++;
-        } while (result.Count() < 8);
 
 
         // part 2
-        index = 0;
-        hash = string.Empty;
-        passcode = string.Empty;
-        result = new List<Char> { '_', '_', '_', '_', '_', '_', '_', '_' };
+        var index = 0;
+        var hash = string.Empty;
+        var result = new List<Char> { '_', '_', '_', '_', '_', '_', '_', '_' };
 
         System.Console.WriteLine("\nPart 2: ");
+
         do
         {
-            passcode = input + index.ToString();
-            hash = GenerateMD5(passcode);
+            hash = NextHash(input, index);
 
             if (hash.StartsWith("00000") && new[] { '0', '1', '2', '3', '4', '5', '6', '7' }.Contains(hash[5]) && result[hash[5] - '0'] == '_')
             {
@@ -46,17 +34,25 @@ public class Day5
 
             index++;
         } while (result.Contains('_'));
-    }
 
-    public static string GenerateMD5(string input)
-    {
 
-        using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+        // helpers
+
+        string NextHash(string input, int index)
         {
-            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
-            byte[] hashBytes = md5.ComputeHash(inputBytes);
+            return GenerateMD5(input + index.ToString());
+        }
 
-            return Convert.ToHexString(hashBytes);
+        string GenerateMD5(string input)
+        {
+
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                return Convert.ToHexString(hashBytes);
+            }
         }
     }
 }
