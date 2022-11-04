e7b052dd65d4ffd7a4472569e3225d9ae77dff53
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-11-04 21:51:48 +0100

    2016 day 7 pt 2

--- 2016/cs/Day7.cs
+++ 2016/cs/Day7.cs
@@ -6,11 +6,19 @@ public class Day7
 {
     public static void Run(string input, string[] lines)
     {
-        var r = lines.Select(l => Parse(l))
-             .Count(parts => parts.Any(p => ContainsAbba(p.Key) && !p.Value) 
+        var one = lines.Select(l => Parse(l))
+             .Count(parts => parts.Any(p => ContainsAbba(p.Key) && !p.Value)
                     && !parts.Any(p => ContainsAbba(p.Key) && p.Value));
 
-        System.Console.WriteLine($"Part 1: {r}");
+        System.Console.WriteLine($"Part 1: {one}");
+
+        var two = lines.Select(l => Parse(l))
+            .Where(parts => parts.Any(p => !p.Value && ABAs(p.Key, false).Any()))
+            .Select(a => new { ParsedLine = a, ABAs = a.Where(part => !part.Value).SelectMany(part => ABAs(part.Key, false))})
+            .Count(aba => aba.ParsedLine.Where(p => p.Value).SelectMany(p => ABAs(p.Key, true)).Intersect(aba.ABAs).Any());
+
+        System.Console.WriteLine($"Part two: {two}");
+
 
         // helpers
         List<KeyValuePair<string, bool>> Parse(string s)
@@ -20,7 +28,7 @@ public class Day7
 
             foreach (var c in s)
             {
-                if (new[]{'[', ']'}.Contains(c))
+                if (new[] { '[', ']' }.Contains(c))
                 {
                     if (buffer.Length > 0) result.Add(new KeyValuePair<string, bool>(buffer.ToString(), c == ']'));
                     buffer.Clear();
@@ -47,7 +55,31 @@ public class Day7
                     return true;
                 }
             }
-            return false;   
+            return false;
+        }
+
+        IEnumerable<string> ABAs(string s, bool reverse)
+        {
+            var result = new List<string>();
+            if (s.Length < 3) return result;
+
+            for (int i = 0; i < s.Length - 2; i++)
+            {
+                var block = s.Substring(i, 3);
+                if (block[0] == block[2] && block[1] != block[2])
+                {
+                    if (reverse)
+                    {
+                        result.Add($"{block[1]}{block[0]}{block[1]}");
+                    }
+                    else
+                    {
+                        result.Add(block);
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
