7cfe344f602b764491377aa0344e9f67746b6d66
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-12-08 21:15:18 +0100

    2022 Day 8 pt. 2

--- 2022/cs/Day8.cs
+++ 2022/cs/Day8.cs
@@ -6,12 +6,16 @@ public class Day8
     {
         if (lines[0].Length != lines.Count()) throw new InvalidDataException("Grid is not square");
 
+        Part1(lines);
+        Part2(lines);
+    }
+
+    private static void Part1(string[] lines)
+    {
+
         var size = lines.Count();
         var grid = new bool[size, size];
         var tallest = -1;
-
-        Print(grid);
-        System.Console.WriteLine();
 
         for (int y = 0; y < size; y++)
         {
@@ -67,23 +71,68 @@ public class Day8
             }
         }
 
-        Print(grid);
-
         var part1 = grid.Cast<bool>().Count(x => x);
         System.Console.WriteLine($"Part 1: {part1}");
 
-        void Print(bool[,] grid)
+    }
+
+    private static void Part2(string[] lines)
+    {
+        var size = lines.Count();
+        var scenicScore = 0;
+
+
+        for (int y = 0; y < size; y++)
         {
-            for (int y = 0; y < size; y++)
+            for (int x = 0; x < size; x++)
             {
-                // rows forwards
-                for (int x = 0; x < size; x++)
-                {
-                    System.Console.Write($"{grid[x, y]} \t");
-                }
-                System.Console.WriteLine();
+                var score = GetScore(lines, x, y);
+                if (score > scenicScore) scenicScore = score;
             }
         }
 
+        System.Console.WriteLine($"Part 2: {scenicScore}");
+
+        int GetScore(string[] grid, int x, int y)
+        {
+            // wat een puinhoop :()
+            
+            int z;
+            var subScores = new List<int>();
+
+            // look up
+            z = 1;
+            while (y - z > 0 && grid[y - z][x] < grid[y][x])
+            {
+                z++;
+            }
+            subScores.Add(y > 0 ? z : 0);
+
+            z = 1;
+            // look left
+            while (x - z > 0 && grid[y][x - z] < grid[y][x])
+            {
+                z++;
+            }
+            subScores.Add(x > 0 ? z : 0);
+
+            // look down
+            z = 1;
+            while (y + z < size - 1 && grid[y + z][x] < grid[y][x])
+            {
+                z++;
+            }
+            subScores.Add(y < size - 1 ? z : 0);
+
+            // look right
+            z = 1;
+            while (x + z < size - 1 && grid[y][x + z] < grid[y][x])
+            {
+                z++;
+            }
+            subScores.Add(x < size - 1 ? z : 0);
+
+            return subScores.Aggregate((a, x) => a * x);
+        }
     }
 }
