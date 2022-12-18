90038ccda1cf3d180adec17b6a0fc901964d4f4b
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-12-18 20:25:32 +0100

    2022 Day 13 pt. 1

--- 2022/cs/Day13.cs
+++ 2022/cs/Day13.cs
@@ -9,34 +9,46 @@ public class Day13
         var pairs = lines.Where(l => !string.IsNullOrWhiteSpace(l))
                         .Select(l => JArray.Parse(l))
                         .Chunk(2)
-                        .Select((chunk, i) => new {Id = i, Pair = chunk})
+                        .Select((chunk, i) => new {Id = i+1, Pair = chunk})
                         .ToList();
 
-        foreach (var p in pairs)
-        {
-            System.Console.WriteLine($"{p.Id}: {Compare(p.Pair[0], p.Pair[1])}");
-        }
+        var part1 = pairs.Where(p => Compare(p.Pair[0], p.Pair[1]) == true)
+                         .Sum(p => p.Id);
 
-        bool Compare(JToken left, JToken right)
+        System.Console.WriteLine($"Part 1: {part1}");
+
+        bool? Compare(JToken left, JToken right)
         {
             if (left.Type == JTokenType.Integer && right.Type == JTokenType.Integer)
             {
                 // direct comparison
+                if ((int)left == (int)right) return null;
                 return (int)right > (int)left;
             }
 
             if (left.Type == JTokenType.Integer && right.Type == JTokenType.Array)
             {
                 left = new JArray { (int)left };
+                return Compare(left, right);
             }
             if (left.Type == JTokenType.Array && right.Type == JTokenType.Integer)
             {
                 right = new JArray { (int)right };
+                return Compare(left, right);
             }
             
             if ((left.Type == JTokenType.Array && right.Type == JTokenType.Array))
             {
-                return Compare(left, right);
+                var len = Math.Min(left.Count(), right.Count());
+
+                for (int i = 0; i < len; i++)
+                {
+                    var result = Compare(left[i]!, right[i]!);
+                    if (result != null) return result;
+                }
+                
+                if (left.Count() == right.Count()) return null;
+                return left.Count() < right.Count();
             }
             
             throw new InvalidOperationException($"Compare failed, left: {left.Type}, right: {right.Type}");
