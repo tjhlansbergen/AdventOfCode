ee23252d2511f82adaab866f95ad98111095b678
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-12-18 21:04:55 +0100

    2022 Day 13 pt. 2

--- 2022/cs/Day13.cs
+++ 2022/cs/Day13.cs
@@ -6,24 +6,49 @@ public class Day13
 {
     public static void Run(string input, string[] lines)
     {
+        // this one turned out well :))
+
+        var c = new Day13Comparer();
+
         var pairs = lines.Where(l => !string.IsNullOrWhiteSpace(l))
                         .Select(l => JArray.Parse(l))
                         .Chunk(2)
-                        .Select((chunk, i) => new {Id = i+1, Pair = chunk})
+                        .Select((chunk, i) => new { Id = i + 1, Pair = chunk })
                         .ToList();
 
-        var part1 = pairs.Where(p => Compare(p.Pair[0], p.Pair[1]) == true)
+        var part1 = pairs.Where(p => c.Compare(p.Pair[0], p.Pair[1]) == -1)
                          .Sum(p => p.Id);
 
         System.Console.WriteLine($"Part 1: {part1}");
 
-        bool? Compare(JToken left, JToken right)
+        var packets = lines.Where(l => !string.IsNullOrWhiteSpace(l))
+                           .Select(l => JArray.Parse(l))
+                           .Select(p => new { Div = false, Packet = p })
+                           .ToList();
+
+        packets.Add(new { Div = true, Packet = JArray.Parse("[[2]]") });
+        packets.Add(new { Div = true, Packet = JArray.Parse("[[6]]") });
+
+        var part2 = packets.OrderBy(p => p.Packet, c)
+                           .Select((p, i) => new { Id = i + 1, p.Div })
+                           .Where(x => x.Div)
+                           .Select(x => x.Id)
+                           .Aggregate((a, x) => a * x);
+
+        System.Console.WriteLine($"Part 2: {part2}");
+    }
+
+    class Day13Comparer : IComparer<JToken>
+    {
+        public int Compare(JToken? left, JToken? right)
         {
+            if (left == null || right == null) throw new NullReferenceException("Left or right was NULL when comparing");
+
             if (left.Type == JTokenType.Integer && right.Type == JTokenType.Integer)
             {
                 // direct comparison
-                if ((int)left == (int)right) return null;
-                return (int)right > (int)left;
+                if ((int)left == (int)right) return 0;
+                return ((int)right > (int)left) ? -1 : 1;
             }
 
             if (left.Type == JTokenType.Integer && right.Type == JTokenType.Array)
@@ -36,7 +61,7 @@ public class Day13
                 right = new JArray { (int)right };
                 return Compare(left, right);
             }
-            
+
             if ((left.Type == JTokenType.Array && right.Type == JTokenType.Array))
             {
                 var len = Math.Min(left.Count(), right.Count());
@@ -44,13 +69,13 @@ public class Day13
                 for (int i = 0; i < len; i++)
                 {
                     var result = Compare(left[i]!, right[i]!);
-                    if (result != null) return result;
+                    if (result != 0) return result;
                 }
-                
-                if (left.Count() == right.Count()) return null;
-                return left.Count() < right.Count();
+
+                if (left.Count() == right.Count()) return 0;
+                return (left.Count() < right.Count()) ? -1 : 1;
             }
-            
+
             throw new InvalidOperationException($"Compare failed, left: {left.Type}, right: {right.Type}");
         }
     }
