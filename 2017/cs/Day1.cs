8d5c8ae8c68eba2623044eea3f08f64ffd3cd271
Tako Lansbergen <takolansbergen@xs4all.nl>
2023-01-09 21:32:01 +0100

    Refactor

--- 2017/cs/Day1.cs
+++ 2017/cs/Day1.cs
@@ -4,12 +4,12 @@ public class Day1
 {
     public static void Run(string input, string[] lines)
     {
-        int GetCount(string line, int shift) => 
+        int GetSum(string line, int shift) => 
             Enumerable.Range(0, line.Length).Where(i => input[i] == input[(i + shift < input.Length) ? i + shift : i + shift - input.Length])
                                             .Select(i => input[i] - '0')
                                             .Sum();
         
-        System.Console.WriteLine($"Part 1: {GetCount(input, 1)}");
-        System.Console.WriteLine($"Part 2: {GetCount(input, input.Length / 2)}");
+        System.Console.WriteLine($"Part 1: {GetSum(input, 1)}");
+        System.Console.WriteLine($"Part 2: {GetSum(input, input.Length / 2)}");
     }
 }
