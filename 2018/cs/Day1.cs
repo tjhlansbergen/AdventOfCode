605e7a1562babade264e4979b85c67dff78c336a
Tako Lansbergen <takolansbergen@xs4all.nl>
2023-04-23 20:08:04 +0200

    2018 Day 1

--- 2018/cs/Day1.cs
+++ 2018/cs/Day1.cs
@@ -4,6 +4,26 @@ public class Day1
 {
     public static void Run(string input, string[] lines)
     {
-        Console.WriteLine(input);
+        var part1 = lines.Select(l => int.Parse(l)).Sum();
+        Console.WriteLine($"Part 1: {part1}");
+
+        var changes = lines.Select(l => int.Parse(l)).ToArray();
+        var freqs = new HashSet<int>();
+        var freq = 0;
+        var index = 0;
+
+        while(true)
+        {
+            freq += changes[index];
+            
+            if (!freqs.Add(freq))
+            {
+                System.Console.WriteLine($"Part 2: {freq}");
+                break;
+            }
+
+            index += 1;
+            if (index == changes.Length) index = 0;
+        }
     }
 }
