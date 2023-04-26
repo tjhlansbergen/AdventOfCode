e7e23277d5ab747fa9e114aecd01718dd71f7cf4
Tako Lansbergen <takolansbergen@xs4all.nl>
2023-04-26 21:18:20 +0200

    Refactor using yield return

--- 2017/cs/Day2.cs
+++ 2017/cs/Day2.cs
@@ -8,24 +8,27 @@ public class Day2
                         .Sum(l => l.Max() - l.Min());
 
         var desc = lines.Select(l => l.Split().Select(c => int.Parse(c)).OrderByDescending(x => x).ToArray());
+        var pt2 = LineResults(desc).Sum();
 
-        var lineResult = new List<int>();
+        System.Console.WriteLine($"Part 1: {pt1}");
+        System.Console.WriteLine($"Part 2: {pt2}");
+    }
+
+    private static IEnumerable<int> LineResults(IEnumerable<int[]> desc)
+    {
         foreach (var line in desc)
         {
             for (int i = 0; i < line.Count() - 1; i++)
             {
                 for (int j = i + 1; j < line.Count(); j++)
                 {
-                    if (line[i] % line[j] == 0) 
+                    if (line[i] % line[j] == 0)
                     {
-                        lineResult.Add(line[i] / line[j]);
+                        yield return line[i] / line[j];
                         break;
                     }
                 }
             }
         }
-        
-        System.Console.WriteLine($"Part 1: {pt1}");
-        System.Console.WriteLine($"Part 2: {lineResult.Sum()}");
     }
 }
