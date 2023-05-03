ae94748f1f5c49d4db5a18d1ea1398dd439e8407
Tako Lansbergen <takolansbergen@xs4all.nl>
2023-05-03 22:05:33 +0200

    2017 day 1 part 2 haskell

--- 2017/haskell/day1.hs
+++ 2017/haskell/day1.hs
@@ -1,7 +1,13 @@
 import System.IO
+import Text.Printf
 
-part1 = do
+main = do
     s <- readFile "../inputs/day1"
-    let x = [ read [s !! x] :: Int | x <- [0..(length s - 1)], s !! x == s !! ( if x + 1 > (length s - 1) then x - length s + 1 else x + 1)]
-    print (sum x)
-    
+    putStrLn ("Part 1: " ++ show (countEqual s 1))
+    putStrLn ("Part 2: " ++ show (countEqual s (length s `div` 2)))
+
+countEqual :: String -> Int -> Int
+countEqual s ahead =
+    sum x
+    where x = [ read [s !! x] :: Int | x <- [0..(length s - 1)], s !! x == s !! ( if x + ahead > (length s - 1) then x - length s + ahead else x + ahead)]
+
