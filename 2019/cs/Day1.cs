1ed1ce5743d11be62d3e03fdd6aa18bac8e658b8
Tako Lansbergen <takolansbergen@xs4all.nl>
2023-05-17 21:48:58 +0200

    2019 Day 1 part 2 csharp

--- 2019/cs/Day1.cs
+++ 2019/cs/Day1.cs
@@ -5,9 +5,35 @@ public class Day1
     public static void Run(string input, string[] lines)
     {
         var part1 = lines.Select(l => int.Parse(l))
-                         .Select(x => (x / 3) -2)
+                         .Select(x => Fuel(x))
                          .Sum();
 
         System.Console.WriteLine($"Part 1: {part1}");
+
+        var part2 = lines.Select(l => int.Parse(l))
+                         .Select(x => CumulativeFuel(x))
+                         .Sum();
+
+        System.Console.WriteLine($"Part 2: {part2}");
+    }
+
+    private static int CumulativeFuel(int mass)
+    {
+        var c = 0;
+        var x = mass;
+
+        while (x > 0)
+        {
+            x = Fuel(x);
+            c+= x;
+        }
+
+        return c;
+    }
+
+    private static int Fuel(int mass)
+    {
+        var f = (mass / 3) - 2;
+        return f > -1 ? f : 0;
     }
 }
