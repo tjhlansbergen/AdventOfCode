69d8369afd9d0276883c5561e519d18924329538
Tako Lansbergen <takolansbergen@xs4all.nl>
2023-08-11 22:02:07 +0200

    2018 Day4 cs setup

---
+++ 2018/cs/Day4.cs
@@ -0,0 +1,51 @@
+namespace AocRunner;
+
+public class Day4
+{
+    public record Guard
+    {
+        public int Id { get; set; }
+        public Dictionary<int, int> Hours { get; set; }
+
+        public Guard(int id)
+        {
+            Id = id;
+            Hours = new Dictionary<int, int>();
+            for (int i = 0; i < 60; i++) Hours[i] = 0;
+        }
+    }
+
+    public static void Run(string input, string[] lines)
+    {
+        var ordered = lines.OrderBy(l => l);
+        var guards = new List<Guard>();
+        Guard? currentGuard = null;
+
+        foreach (var line in ordered)
+        {
+            var s = line.Split(']');
+            if (s[1].StartsWith(" Guard")) ParseGuard(line);
+            else ParseSleep(line);
+        }
+
+        void ParseGuard(string line)
+        {
+            var id = int.Parse(line.Split('#')[1].Split(" begins")[0]);
+            if (guards.Select(g => g.Id).Contains(id)) 
+            {
+                currentGuard = guards.Single(g => g.Id == id);
+            }
+            else
+            {
+                currentGuard = new Guard(id); 
+                guards.Add(currentGuard);
+            } 
+        }
+
+        void ParseSleep(string line)
+        {
+            
+        }
+
+    }
+}
