adfdb9ea82c1d50f0cae5a1d13f2bdc64aac610b
Tako Lansbergen <lansta@uptrends.com>
2023-11-07 17:04:27 +0100

    2017 day 6 1&2 CS

--- 2017/cs/Day6.cs
+++ 2017/cs/Day6.cs
@@ -4,23 +4,39 @@ public class Day6
 {
     public static void Run(string input, string[] lines)
     {
-        var blocks = input.Split('\t').Select(s => int.Parse(s)).ToArray();
-        var size = blocks.Count();
+        // NON-OPTIMIZED! :)
+
+        var blocks = input.Split('\t').Select(s => int.Parse(s)).ToList();
+        var size = blocks.Count;
         
-        var results = new List<int[]> { blocks };
+        var results = new List<List<int>> { blocks };
 
         do {
-            var last = results.Last();
-            var max = last.Max();
-            var dist = last.First(block => block == max);
-            
-            
-        } while (true);
+            var last = new List<int>(results.Last());
 
+            var maxValue = last.Max();
+            var maxBlock = last.First(block => block == maxValue);
+            var maxBlockIndex = last.IndexOf(maxBlock);
+
+            last[maxBlockIndex] = 0;
+            int i = maxBlockIndex;
+
+            for (int c = 0; c < maxValue; c++)
+            {
+                i = Next(i, last.Count);
+                last[i]++;
+            }
+
+            results.Add(last);
+        } while (results.Count == results.Select(r => string.Join('.', r)).Distinct().Count());
+
+        System.Console.WriteLine($"Part 1: {results.Count - 1}" );
+
+        var final = string.Join('.', results.Last());
+        var first = results.IndexOf(results.First(r => string.Join('.', r) == final));
+
+        System.Console.WriteLine($"Part 2: {results.Count - 1 - first}" );
     }
 
-    public int Next(int i, int size)
-    {
-        return (i == size - 1) ? 0 : i++;
-    }
+    public static int Next(int i, int size) => (i == size - 1) ? 0 : i+1;
 }
