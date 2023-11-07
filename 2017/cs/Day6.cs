782fa54d78da564d284ea1c20f8090670141e758
Tako Lansbergen <takolansbergen@xs4all.nl>
2023-11-07 21:38:20 +0100

    2017 day 6 cs optimized using HashSet

--- 2017/cs/Day6.cs
+++ 2017/cs/Day6.cs
@@ -4,38 +4,29 @@ public class Day6
 {
     public static void Run(string input, string[] lines)
     {
-        // NON-OPTIMIZED! :)
-
-        var blocks = input.Split('\t').Select(s => int.Parse(s)).ToList();
-        var size = blocks.Count;
+        var current = input.Split('\t').Select(s => int.Parse(s)).ToList();
+        var size = current.Count;
         
-        var results = new List<List<int>> { blocks };
+        var results = new HashSet<string>();
 
-        do {
-            var last = new List<int>(results.Last());
+        while (results.Add(string.Join('.', current))) 
+        {
+            var maxValue = current.Max();
+            var maxBlock = current.First(block => block == maxValue);
+            var maxBlockIndex = current.IndexOf(maxBlock);
 
-            var maxValue = last.Max();
-            var maxBlock = last.First(block => block == maxValue);
-            var maxBlockIndex = last.IndexOf(maxBlock);
-
-            last[maxBlockIndex] = 0;
+            current[maxBlockIndex] = 0;
             int i = maxBlockIndex;
 
             for (int c = 0; c < maxValue; c++)
             {
-                i = Next(i, last.Count);
-                last[i]++;
+                i = Next(i, size);
+                current[i]++;
             }
+        }
 
-            results.Add(last);
-        } while (results.Count == results.Select(r => string.Join('.', r)).Distinct().Count());
-
-        System.Console.WriteLine($"Part 1: {results.Count - 1}" );
-
-        var final = string.Join('.', results.Last());
-        var first = results.IndexOf(results.First(r => string.Join('.', r) == final));
-
-        System.Console.WriteLine($"Part 2: {results.Count - 1 - first}" );
+        System.Console.WriteLine($"Part 1: {results.Count}" );
+        System.Console.WriteLine($"Part 2: {results.Count - results.ToList().IndexOf(results.Single(r => r == string.Join('.', current)))}" );
     }
 
     public static int Next(int i, int size) => (i == size - 1) ? 0 : i+1;
