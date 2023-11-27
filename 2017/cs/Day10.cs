bbff4a09b8143398f4689302ed7d55ca655eec53
Tako Lansbergen <takolansbergen@xs4all.nl>
2023-11-27 21:35:04 +0100

    2017 day 10 part 1 cs

--- 2017/cs/Day10.cs
+++ 2017/cs/Day10.cs
@@ -4,5 +4,40 @@ public class Day10
 {
     public static void Run(string input, string[] lines)
     {
+        var strng = new LinkedList<int>(Enumerable.Range(0, 256));
+        var lengths = input.Split(',').Select(i => int.Parse(i.Trim()));
+
+        var current = strng.First;
+        var skip = 0;
+
+        //System.Console.WriteLine(string.Join(',', strng));
+
+        foreach (var length in lengths)
+        {
+            var reversedPart = new LinkedList<int>();
+            var start = current;
+
+            for (int i = 0; i < length; i++)
+            {
+                reversedPart.AddFirst(current!.Value);
+                current = current.Next ?? strng.First;
+            }
+
+            current = start;
+
+            foreach (var item in reversedPart)
+            {    
+                current!.Value = item;
+                current = current.Next ?? strng.First;
+            }
+
+            for (int i = 0; i < skip; i++) { current = current!.Next ?? strng.First; }
+
+            skip++;
+
+            //System.Console.WriteLine(string.Join(',', strng));
+        }
+
+        System.Console.WriteLine($"Part 1: {strng!.First!.Value * strng.First.Next!.Value }");
     }
 }
