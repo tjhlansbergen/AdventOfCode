2fe6f3edf51b998d0c71ac2c452784b30e7bee32
Tako Lansbergen <lansta@uptrends.com>
2023-12-07 11:20:30 +0100

    2023 day 5 brute force solution

--- 2023/cs/Day5.cs
+++ 2023/cs/Day5.cs
@@ -1,12 +1,28 @@
+using System.ComponentModel.Design;
 using System.Data;
 
 namespace AocRunner;
 
 public class Day5
 {
+    public class Mapper
+    {
+        public required Map[] Maps { get; set; }
+
+        public long Process(long seed, bool reverse = false)
+        {
+            foreach (var map in Maps)
+            {
+                seed = map.Process(seed);
+            }
+
+            return seed;
+        }
+    }
+
     public class Map
     {
-        public List<Range> Ranges { get; set; } = new ();
+        public List<Range> Ranges { get; set; } = new();
 
         public long Process(long seed)
         {
@@ -25,32 +41,66 @@ public class Day5
 
     public static void Run(string input, string[] lines)
     {
+        var maps = input.Split("\r\n\r\n").Select(block => Parse(block)).Where(map => map != null).Reverse().ToArray();     // in reverse, to traverse the maps buttom to top
+        var mapper = new Mapper { Maps = maps };
+
+        var allRanges = maps.SelectMany(m => m.Ranges);
         var seeds = lines[0].Split(": ")[1].Split(' ', StringSplitOptions.TrimEntries).Select(s => long.Parse(s));
-        var maps = input.Split("\r\n\r\n").Select(block => Parse(block)).Where(map => map != null);
-        var lowest = long.MaxValue;
 
-        foreach (var seed in seeds)
+        // start with the 'lowest' possbile outcome, test all possible outcomes until one matches an original seed
+        var stop = false;
+        foreach (var range in allRanges.OrderBy(range => range.From))
         {
-            var result = seed;
-
-            foreach (var map in maps)
+            for (long i = range.From; i < range.To; i++)
             {
-                result = map.Process(result);
+                var result = mapper.Process(i);
+
+                if (seeds.Any(seed => seed == result))
+                {
+                    System.Console.WriteLine($"Part 1: {i}");
+                    stop = true;
+                    break; 
+                }
             }
-            lowest = Math.Min(lowest, result);
+            if (stop) break;
         }
-        System.Console.WriteLine($"Part 1: {lowest}");
+
+        var seedRanges = lines[0]
+            .Split(": ")[1]
+            .Split(' ', StringSplitOptions.TrimEntries)
+            .Select(s => long.Parse(s))
+            .Chunk(2)
+            .Select(pair => (pair[0], pair[0] + pair[1]))
+            .ToArray();
+
+        // start with the 'lowest' possbile outcome, test all possible outcomes until one matches an original seed from any of the seed ranges
+        stop = false;
+        foreach (var range in allRanges.OrderBy(range => range.From))
+        {
+            for (long i = range.From; i < range.To; i++)
+            {
+                var result = mapper.Process(i);
+
+                if (seedRanges.Any(sr => sr.Item1 <= result && result < sr.Item2))
+                {
+                    System.Console.WriteLine($"Part 2: {i}");
+                    stop = true;
+                    break; 
+                }
+            }
+            if (stop) break;
+        }
     }
 
     public static Map Parse(string block)
     {
-        if (block.StartsWith("seeds: ")) return null;
+        if (block.StartsWith("seeds: ")) return null!;
 
         var ranges = block.Split("\r\n", StringSplitOptions.TrimEntries)
                 .Where(line => !line.Contains(':'))
-                .Select(line => line.Split(' ').Select(i => long.Parse(i)).ToArray())                
-                .Select(longs => new Range { From =  longs[1], To = longs[1] + longs[2], Conversion = longs[0] - longs[1] });
+                .Select(line => line.Split(' ').Select(i => long.Parse(i)).ToArray())
+                .Select(longs => new Range { From = longs[0], To = longs[0] + longs[2], Conversion = longs[1] - longs[0] });    // this destination and source, since we will be traversing the maps in reverse
 
-        return new Map{ Ranges = ranges.ToList() };
+        return new Map { Ranges = ranges.ToList() };
     }
 }
