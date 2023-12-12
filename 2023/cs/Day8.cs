38caa176d3e93b30c0020e7ecedd83f1f7407193
Tako Lansbergen <takolansbergen@xs4all.nl>
2023-12-12 21:55:42 +0100

    2023 day 8 part 2 cs

--- 2023/cs/Day8.cs
+++ 2023/cs/Day8.cs
@@ -9,7 +9,7 @@ public class Day8
 
         var currentDirection = directions.First;
         var here = "AAA";
-        var count = 0; 
+        var count = 0;
 
         while (here != "ZZZ")
         {
@@ -19,13 +19,51 @@ public class Day8
         }
 
         System.Console.WriteLine($"Part 1: {count}");
+
+        //
+
+
+        var starts = map.Keys.Where(k => k[2] == 'A');
+        var counts = new List<long>();
+
+        foreach (var start in starts)
+        {
+            currentDirection = directions.First;
+            here = start;
+            count = 0;
+
+            while (here[2] != 'Z')
+            {
+                here = map[here][currentDirection!.Value];
+                currentDirection = currentDirection!.Next ?? directions.First;
+                count++;
+            }
+            counts.Add(count);
+        }
+
+        System.Console.WriteLine($"Part 2: {LCM(counts.ToArray())}");
+
     }
 
     private static Dictionary<string, string[]> ParseMap(IEnumerable<string> lines)
     {
         return lines.Select(l => l.Replace('=', ',').Replace('(', ' ').Replace(')', ' '))
              .Select(l => l.Split(',', StringSplitOptions.TrimEntries))
-             .Select(splits => new { Key = splits[0], Value = new [] {splits[1], splits[2]} } )
+             .Select(splits => new { Key = splits[0], Value = new[] { splits[1], splits[2] } })
              .ToDictionary(x => x.Key, x => x.Value);
+    }
+
+    // https://stackoverflow.com/questions/147515/least-common-multiple-for-3-or-more-numbers/29717490#29717490
+    private static long LCM(long[] numbers)
+    {
+        return numbers.Aggregate(lcm);
+    }
+    private static long lcm(long a, long b)
+    {
+        return Math.Abs(a * b) / GCD(a, b);
+    }
+    private static long GCD(long a, long b)
+    {
+        return b == 0 ? a : GCD(b, a % b);
     }
 }
