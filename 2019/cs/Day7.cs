05e5d87c8a37d433e19c7bce1b8923d10b16ce86
Tako Lansbergen <takolansbergen@xs4all.nl>
2024-12-20 13:47:18 +0100

    Stub 2024

---
+++ 2019/cs/Day7.cs
@@ -0,0 +1,9 @@
+namespace AocRunner;
+
+public class Day7
+{
+    public static void Run(string input, string[] lines)
+    {
+
+    }
+}
---
+++ 2024/cs/Day1.cs
@@ -0,0 +1,15 @@
+namespace AocRunner;
+
+public class Day1
+{
+    public static void Run(string input, string[] lines)
+    {
+        System.Console.WriteLine($"Part 1: ");
+
+        foreach(var line in lines)
+        {
+            System.Console.WriteLine(line);
+        }
+    
+    }
+}
