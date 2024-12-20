c4d1a52210f6e455de3c18a242ac24c61528113b
Tako Lansbergen <takolansbergen@xs4all.nl>
2024-12-20 14:02:03 +0100

    2014 day 1

--- 2024/cs/Day1.cs
+++ 2024/cs/Day1.cs
@@ -1,15 +1,21 @@
+
 namespace AocRunner;
 
 public class Day1
 {
     public static void Run(string input, string[] lines)
     {
-        System.Console.WriteLine($"Part 1: ");
+        var parsed = lines.Select(l => l.Split("   ")).Select(split => new { Left = int.Parse(split[0]), Right = int.Parse(split[1]) }).ToList();
+        var left = parsed.Select(p => p.Left).OrderByDescending(x => x).ToList();
+        var right = parsed.Select(p => p.Right).OrderByDescending(x => x).ToList();
 
-        foreach(var line in lines)
-        {
-            System.Console.WriteLine(line);
-        }
+        var part1 = left.Zip(right).Select(p => Math.Abs(p.First - p.Second)).Sum();
+
+        System.Console.WriteLine($"Part 1: {part1}");   
+
+        var part2 = left.Select(l => l * right.Count(r => r == l)).Sum();
+
+        System.Console.WriteLine($"Part 2: {part2}");   
     
     }
 }
