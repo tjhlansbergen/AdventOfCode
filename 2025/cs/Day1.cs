6bae23c76fed326b44d16a0954f65a9b98a9a1b0
Tako Lansbergen <takolansbergen@xs4all.nl>
2025-12-08 21:19:54 +0100

    2025 day1 part 2

--- 2025/cs/Day1.cs
+++ 2025/cs/Day1.cs
@@ -5,16 +5,49 @@ public class Day1
 {
     public static void Run(string input, string[] lines)
     {
-        foreach (var p in Positions(lines))
-        {
-            Console.WriteLine(p);
-        }
+        var part1 = EndPositions(lines).Count(p => p == 0);
+        Console.WriteLine($"Part 1: {part1}");
 
-        var day1 = Positions(lines).Count(p => p == 0);
-        Console.WriteLine($"Day 1: {day1}");    
+        var part2 = AllPositions(lines);
+        Console.WriteLine($"Part 2: {part2}");
     }
 
-    public static IEnumerable<int> Positions(string[] lines)
+    public static int AllPositions(string[] lines)
+    {
+        var p = 50;
+        var count = 0;
+
+        foreach (var line in lines)
+        {
+            var move = Move(line);
+            for (int i = 0; i < Math.Abs(move); i++)
+            {
+                p = Rotate(p, move < 0);
+                if (p == 0) count++;
+            }   
+        }
+
+
+        return count;
+    }
+
+    static int Rotate(int p, bool left)
+    {
+        if (left)
+        {
+            p--;
+            if (p < 0) p = 99;
+            return p;
+        }
+        else
+        {
+            p++;
+            if (p >= 100) p = 0;
+            return p;
+        }
+    }
+
+    public static IEnumerable<int> EndPositions(string[] lines)
     {
         var p = 50;
         yield return p;
@@ -26,11 +59,11 @@ public class Day1
             while (p >= 100) p -= 100;
             yield return p;
         }
+    }
 
-        static int Move(string l)
-        {
-            var distance = int.Parse(l[1..]);
-            return l[..1] == "L" ? -distance : distance;
-        }
+    private static int Move(string l)
+    {
+        var distance = int.Parse(l[1..]);
+        return l[..1] == "L" ? -distance : distance;
     }
 }
