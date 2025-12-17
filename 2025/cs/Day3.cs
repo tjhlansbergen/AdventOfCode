5f84001ee15e680ac7446501db2a74d21591d9e4
Tako Lansbergen <takolansbergen@xs4all.nl>
2025-12-17 13:52:44 +0100

    2025 day 3 part 2

--- 2025/cs/Day3.cs
+++ 2025/cs/Day3.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace AocRunner;
 
 public class Day3
@@ -6,6 +8,9 @@ public class Day3
     {
         var part1 = lines.Sum(Joltage);
         Console.WriteLine($"Part 1: {part1}");
+
+        var part2 = lines.Sum(Joltage2);
+        Console.WriteLine($"Part 2: {part2}");
     }
 
     public static int Joltage(string bank)
@@ -15,5 +20,22 @@ public class Day3
         var a = batts[..^1].OrderByDescending(b => b).First();
         var b = batts[(batts.IndexOf(a) + 1)..].OrderByDescending(b => b).First();
         return int.Parse(string.Join("", [a, b]));
+    }
+
+    public static long Joltage2(string bank)
+    {
+        var batts = bank.Select(c => c - '0').ToList();
+
+        var result = new StringBuilder();
+
+        for (int i = 0; i < 12; i++)
+        {
+            var peek = batts[..^(11-i)];
+            var a = peek.OrderByDescending(b => b).First();
+            batts = batts[(batts.IndexOf(a) + 1)..];
+            result.Append(a);
+        }
+
+        return long.Parse(result.ToString());
     }
 }
